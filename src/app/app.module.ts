1c369da9be7718f65663b6e0fb5122f49e1482f7
Thibaut Delplanque <t.delplanque92@gmail.com>
2021-11-08 08:57:42 +0100

    DEV : init base project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ItemComponent } from './modules/application/item/item.component';
+import { ItemModule } from './modules/application/item/item.module';
+import { HomeComponent } from './modules/general/home/home.component';
+import { ContactComponent } from './modules/general/contact/contact.component';
+import { AboutComponent } from './modules/general/about/about.component';
+import { SigninComponent } from './modules/general/signin/signin.component';
+import { NotFoundComponent } from './modules/general/not-found/not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    SigninComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ItemModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
