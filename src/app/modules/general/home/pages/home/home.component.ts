12f30bf3a4318fadfb5ff9c8d1d326d612f6c1af
Thibaut Delplanque <t.delplanque92@gmail.com>
2021-11-19 17:01:27 +0100

    DEV: nettoyage du code

--- src/app/modules/general/home/pages/home/home.component.ts
+++ src/app/modules/general/home/pages/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from '../../../../../../environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -8,49 +7,7 @@ import { environment } from '../../../../../../environments/environment';
 })
 export class HomeComponent implements OnInit {
 
-  name = environment.application.name;
-  angular = environment.application.angular;
-  bootstrap = environment.application.bootstrap;
-  fontawesome = environment.application.fontawesome;
-
-  features: any;
-
-  constructor() {
-    this.features =
-      [
-        {
-          name: 'Bootstrap prototype',
-          description: 'Bootstrap Prototype Description',
-          icon: 'fab fa-bootstrap',
-          link: 'bootstrap-prototype'
-        },
-        {
-          name: 'Reactive Form',
-          description: 'Reactive Form Description',
-          icon: 'fab fa-bootstrap',
-          link: 'reactive-form'
-        },
-        {
-          name: 'Services',
-          description: 'Services Description',
-          icon: 'fab fa-bootstrap',
-          link: 'services'
-        },
-        {
-          name: 'Components',
-          description: 'Components Description',
-          icon: 'fab fa-bootstrap',
-          link: 'components'
-        },
-        {
-          name: 'Template Driven Forms',
-          description: 'Template Driven Forms Description',
-          icon: 'fab fa-bootstrap',
-          link: 'template-driven-forms'
-        },
-      ];
-
-  }
+  constructor() { }
 
   ngOnInit(): void {
   }
