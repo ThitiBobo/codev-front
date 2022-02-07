a89b8247ae7b5dbf6d9d450712334b9c3d7228eb
Thibaut Delplanque <t.delplanque92@gmail.com>
2022-02-08 00:32:31 +0100

    DEV : footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+const GITHUB_ACCOUNTS = [
+  {nameTag: "Robin-Fe", link:"https://github.com/Robin-Fe"},
+  {nameTag: "AlexandreMoine", link:"https://github.com/AlexandreMoine"},
+  {nameTag: "ThitiBobo", link:"https://github.com/ThitiBobo"},
+];
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+
+  github_accounts = GITHUB_ACCOUNTS;
+  faGithub = faGithub;
 
   constructor() { }
 
