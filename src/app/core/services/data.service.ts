6e716a6d29d624edf269ec79cb8bb4baa94bd65e
Thibaut Delplanque <t.delplanque92@gmail.com>
2022-02-07 22:56:23 +0100

    DEV : dataservice

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,11 +1,12 @@
 import {ApiBaseService} from "./api-base.service";
-import {Data} from "../models/data";
+import {Data, DataAdapter} from "../models/data";
 //import * as PATH from "path";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {catchError, map, retry} from "rxjs/operators";
 import {Injectable} from "@angular/core";
 import {DataList, DataListAdapter} from "../models/data-list";
+import {DataDetail, DataDetailAdapter} from "../models/data-detail";
 
 @Injectable({
   providedIn: 'root'
@@ -24,7 +25,25 @@ export class DataService extends ApiBaseService<Data> {
         retry(3),
         catchError(this.handleError<DataList>('data.list', new DataList([], [], [])))
       ).pipe(
-        map( value => DataListAdapter.adapt(value))
-      );
+        map(value => DataListAdapter.adapt(value))
+      )
+  }
+
+  get(code: string): Observable<Data> {
+    const url = this.apiUrl + this.path + '/' + code
+    return this.http.get<any>(url).pipe(
+      map(value => DataAdapter.adapt(value))
+    )
+  }
+
+  getConsumption(code: string, rows: number, start?: Date, end?: Date): Observable<DataDetail>{
+    var url = this.apiUrl + this.path + '/' + code + 'period'
+    url = url + '?number=' + rows
+    if (start) url = url + "&start=" + start.toString()
+    if (end) url = url + "&end=" + end.toString()
+
+    return this.http.get(url).pipe(
+      map( value => DataDetailAdapter.adapt(value))
+    )
   }
 }
