60a2e3456dd5f178fc37031afa214856ed02c4ad
Thibaut Delplanque <t.delplanque92@gmail.com>
2022-02-07 23:22:23 +0100

    DEV : mise en forme page login

--- src/app/modules/account/account.module.ts
+++ src/app/modules/account/account.module.ts
@@ -5,17 +5,31 @@ import { AccountRoutingModule } from './account-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import {MatIconModule} from "@angular/material/icon";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
 
 
 @NgModule({
   declarations: [
     LoginComponent,
     RegisterComponent,
-    ProfileComponent
+    ProfileComponent,
   ],
   imports: [
     CommonModule,
-    AccountRoutingModule
+    AccountRoutingModule,
+
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+
   ]
 })
 export class AccountModule { }
--- src/app/modules/account/pages/login/login.component.ts
+++ src/app/modules/account/pages/login/login.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from "@angular/forms";
+import {ErrorStateMatcher} from "@angular/material/core";
+import {ActivatedRoute, Router} from "@angular/router";
+import {AuthService} from "../../../../core/services/auth.service";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit{
 
-  constructor() { }
+  submitted = false;
+  loading = false;
+  returnUrl: string;
 
-  ngOnInit(): void {
+  hide = true;
+  matcher = new MyErrorStateMatcher();
+
+  loginForm = new FormGroup({
+    login:    new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.min(3)]),
+  });
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService)
+  {
+    this.returnUrl = "/"
   }
 
+  ngOnInit(): void {
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    if (this.returnUrl == this.router.url){
+      this.returnUrl = '/'
+    }
+  }
+
+  get emailInput() { return this.loginForm.get('email'); }
+  get passwordInput() { return this.loginForm.get('password'); }
+
+  onSubmit() {
+    this.submitted = true;
+
+    console.log(this.returnUrl)
+    this.loading = true;
+    this.authService.login(this.loginForm.value.login, this.loginForm.value.password)
+      .subscribe(data => {
+        this.router.navigate([this.returnUrl])
+      })
+  }
+
+}
+
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
 }
