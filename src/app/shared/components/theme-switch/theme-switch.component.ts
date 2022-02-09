c41588c9dd178882f425ef199d33fd9cd792bbd9
Thibaut Delplanque <t.delplanque92@gmail.com>
2022-02-09 15:12:56 +0100

    DEV : theme

--- src/app/shared/components/theme-switch/theme-switch.component.ts
+++ src/app/shared/components/theme-switch/theme-switch.component.ts
@@ -12,19 +12,19 @@ export class ThemeSwitchComponent{
   private static readonly DARK_THEME_CLASS = 'dark-theme';
 
   constructor(@Inject(DOCUMENT) private document: Document, public service: ThemeService) {
-    //service.isDarkMode()? this.selectDarkTheme(): this.selectLightTheme();
+    service.isDarkMode()? this.selectDarkTheme(): this.selectLightTheme();
   }
 
   toggle() {
-    //this.service.switchTheme();
-    //this.service.isDarkMode()? this.selectDarkTheme(): this.selectLightTheme();
+    this.service.switchTheme();
+    this.service.isDarkMode()? this.selectDarkTheme(): this.selectLightTheme();
   }
 
   public selectDarkTheme(): void {
-    //this.document.documentElement.classList.add(ThemeSwitchComponent.DARK_THEME_CLASS);
+    this.document.documentElement.classList.add(ThemeSwitchComponent.DARK_THEME_CLASS);
   }
 
   public selectLightTheme(): void {
-    //this.document.documentElement.classList.remove(ThemeSwitchComponent.DARK_THEME_CLASS);
+    this.document.documentElement.classList.remove(ThemeSwitchComponent.DARK_THEME_CLASS);
   }
 }
